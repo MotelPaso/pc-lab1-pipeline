508bef2b4a8c0b25707c1d094544eaed49e0b11a
Andresalitre <andresbrt17@gmail.com>
2026-09-23 03:16:38 -0300

    Agregar procesamiento de datos y obtener fps en cargar_datos

--- src/pc_lab1_pipeline/__init__.py
+++ src/pc_lab1_pipeline/__init__.py
@@ -1,6 +1,7 @@
 '''
 Interfaz general para el pipeline
 '''
+from .clases import Observacion
 
 def main() -> None:
     '''
@@ -12,4 +13,37 @@ def cargar_datos(arch) -> None:
     '''
     cargar_datos
     '''
+    fps = 0
+    i = 0
+    
+    for linea in arch:
+        linea = linea.strip()
+        
+        if linea == "": #saltar vacios
+            continue
+        
+        if linea.startswith("#"): # Obtener el framerate del archivo
+            if "framerate" in linea.lower(): 
+                    obtenerFps = linea.split(":")
+                    fps = float(obtenerFps[1].strip())
+            continue # Saltar si la linea no tiene "framerate"
+        
+        partes = linea.split("\t") # Split de cualquier cantidad de espacios
+        
+        if len(partes) < 4: # Saltar la primera linea que no corresponde a los datos
+            continue
+        
+        try:
+            id = int(partes[0])
+            frame = int(partes[1])
+            pos_x = float(partes[2])
+            pos_y = float(partes[3])
+        except ValueError:
+            continue
+        
+        observacion = Observacion(pos_x, pos_y, frame)
+        
+        i += 1
+        print(i, id, frame , pos_x, pos_y)
+    
     print("hola dou")
